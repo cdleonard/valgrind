3754de75df98af2c3aef05b7f1262911f5df42aa
Crestez Dan Leonard <lcrestez@ixiacom.com>
2015-03-30 23:27:15 +0300

    mips64n32: Fix __VKI_ARCH_SI_PREAMBLE_SIZE

--- include/vki/vki-mips64-linux.h
+++ include/vki/vki-mips64-linux.h
@@ -908,7 +908,13 @@ typedef union vki_sigval {
 } vki_sigval_t;
 
 #ifndef __VKI_ARCH_SI_PREAMBLE_SIZE
+#if defined(VGABI_64)
 #define __VKI_ARCH_SI_PREAMBLE_SIZE (4 * sizeof(int))
+#elif defined(VGABI_N32)
+#define __VKI_ARCH_SI_PREAMBLE_SIZE (3 * sizeof(int))
+#else
+#error unknown mips64 abi
+#endif
 #endif
 
 #define VKI_SI_MAX_SIZE 128
